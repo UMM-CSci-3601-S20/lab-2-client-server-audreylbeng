34435172926d15eb2ccf5806efc9be1796bc0c33
Audrey Le Meur <lemeu001@umn.edu>
2020-02-17 11:26:49 -0600

    Filter by key word in body
    Issue #21

--- server/src/main/java/umm3601/todo/TodoDatabase.java
+++ server/src/main/java/umm3601/todo/TodoDatabase.java
@@ -19,7 +19,7 @@ public class TodoDatabase {
       InputStreamReader reader = new InputStreamReader(getClass().getResourceAsStream(todoDataFile));
       allTodos = gson.fromJson(reader, Todo[].class);
     }
-  
+
     public int size() {
       return allTodos.length;
     }
@@ -42,7 +42,7 @@ public class TodoDatabase {
      * @return an array of all the todos matching the given criteria
      */
     public Todo[] listTodos(Map<String, List<String>> queryParams) {
-        
+
         Todo[] filteredTodos = allTodos;
 
         //insert filters here
@@ -54,8 +54,27 @@ public class TodoDatabase {
           }
           filteredTodos = Arrays.copyOfRange(filteredTodos, 0, returnLength);
         }
-    
+
+        //Filter Category
+        if (queryParams.containsKey("contains")) {
+          String targetBody = queryParams.get("contains").get(0);
+          filteredTodos = searchBody(filteredTodos, targetBody);
+        }
+
         return filteredTodos;
+    }
+
+    /**
+     *
+     * Get an array of all the todos containing the target body.
+     *
+     * @param todos     the list of todos to search by body
+     * @param targetBody the target body to look for
+     * @return an array of all the todos from the given list that have the target
+     *         body
+     */
+    public Todo[] searchBody(Todo[] todos, String targetString) {
+      return Arrays.stream(todos).filter(x -> x.body.toLowerCase().contains(targetString.toLowerCase())).toArray(Todo[]::new);
     }
 
 }
--- server/src/test/java/umm3601/todo/FilterTodoByBodyFromDB.java
+++ server/src/test/java/umm3601/todo/FilterTodoByBodyFromDB.java
@@ -17,17 +17,17 @@ import umm3601.Server;
 public class FilterTodoByBodyFromDB {
 
     @Test
-    public void FilterBySint() throws IOException {
+    public void FilterByPeriod() throws IOException {
         TodoDatabase db = new TodoDatabase("/todos.json");
         Map<String, List<String>> queryParams = new HashMap<>();
 
-        queryParams.put("contains", Arrays.asList(new String[] {"sint"}));
+        queryParams.put("contains", Arrays.asList(new String[] {"."}));
         Todo[] sintTodos = db.listTodos(queryParams);
 
-        assertEquals(97, sintTodos.length, "Wrong number of todos returned.");
+        assertEquals(300, sintTodos.length, "Wrong number of todos returned.");
         assertEquals("58895985a22c04e761776d54", sintTodos[0]._id, "First todo returned was incorrect.");
         for(Todo t: sintTodos){
-            assertTrue(t.body.contains("sint"), "One of the returned todos doesn't contain \"sint\".");
+            assertTrue(t.body.toLowerCase().contains("."), "One of the returned todos doesn't contain \"sint\".");
         }
     }
 
@@ -39,10 +39,10 @@ public class FilterTodoByBodyFromDB {
         queryParams.put("contains", Arrays.asList(new String[] {"nisi"}));
         Todo[] nisiTodos = db.listTodos(queryParams);
 
-        assertEquals(97, nisiTodos.length, "Wrong number of todos returned.");
+        assertEquals(83, nisiTodos.length, "Wrong number of todos returned.");
         assertEquals("58895985186754887e0381f5", nisiTodos[0]._id, "First todo returned was incorrect.");
         for(Todo t: nisiTodos){
-            assertTrue(t.body.contains("nisi"), "One of the returned todos doesn't contain \"nisi\".");
+            assertTrue(t.body.toLowerCase().contains("nisi"), "One of the returned todos doesn't contain \"nisi\".");
         }
     }
 
